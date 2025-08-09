852b850b97e02a4ee2cb7d136dd66a8f713be2b6
LullabiesGr <info@lullabies.gr>
2025-08-09 12:24:22 +0300

    Updated CopyPhotoStyleMode.tsx

--- src/components/CopyPhotoStyleMode.tsx
+++ src/components/CopyPhotoStyleMode.tsx
@@ -14,7 +14,7 @@ interface CopyPhotoStyleModeProps {
 const API_URL =
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:8000"
-    : "https://438aeaff2b7a.ngrok-free.app";
+    : "https://b455dac5621c.ngrok-free.app";
 
 const CopyPhotoStyleMode: React.FC<CopyPhotoStyleModeProps> = ({ onBack }) => {
   const { photos, currentAlbumName } = usePhoto();

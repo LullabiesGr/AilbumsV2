f9fb863faba7ee2907af795996b5df6a7f8b9070
LullabiesGr <info@lullabies.gr>
2025-08-22 11:22:52 +0300

    Updated api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,7 +7,7 @@ import { DuplicateCluster } from '../types';
 const API_URL =
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:8000"
-    : "https://e2d5f43272e5.ngrok-free.app";
+    : "https://71d2da8f7b58.ngrok-free.app";
 
 // Helper function to fetch actual file data from URL
 async function fileFromUrl(url: string, filename: string): Promise<File> {

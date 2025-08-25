2ccc3254d6367c7c06e2eb9a267052e12f32b35f
LullabiesGr <info@lullabies.gr>
2025-08-25 11:15:51 +0300

    Updated stripe-config.ts

--- src/stripe-config.ts
+++ src/stripe-config.ts
@@ -58,12 +58,12 @@ export const products: Product[] = [
     ]
   },
   {
-    id: 'prod_S8XTzYuxaHId3q',
-    priceId: 'price_1REGOlCtJc6njTYQF3WdxPX6',
+    id: 'prod_Svn8TJ71J5WFhl',
+    priceId: 'price_1RzvYOCtJc6njTYQhEjD0wVH',
     name: 'Ailbums Credits',
     description: 'Extra Credits',
-    price: 4.00,
-    currency: 'EUR',
+    price: 4.49,
+    currency: 'USD',
     mode: 'payment',
     features: [
       'Additional credits',
